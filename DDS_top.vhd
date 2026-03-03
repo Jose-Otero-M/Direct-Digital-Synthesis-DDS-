800f8f7dad9b73b7503140e0ef1d9dc13e83bfe6
Jose-Otero-M <otero.martinez.jose.2000@gmail.com>
2026-03-02 18:43:13 -0600

    Updated comments for DDS_top.vhd

--- DDS_top.vhd
+++ DDS_top.vhd
@@ -5,20 +5,21 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity DDS is
     generic(
-        FTW_WIDTH : natural := 32;
+        FTW_WIDTH : natural := 32;        -- Frequency Tuning Word width.
         PHASE_WIDTH : natural := 10;
         AMP_WIDTH  : natural := 10
     );
     Port ( CLK      : in STD_LOGIC;
            RST_n    : in STD_LOGIC;
            ENABLE   : in STD_LOGIC;
-           FTW_IN   : in unsigned(FTW_WIDTH-1 downto 0);
-           POW_IN   : in unsigned(PHASE_WIDTH-1 downto 0);
-           OUT_SQ   : out std_logic;
+           FTW_IN   : in unsigned(FTW_WIDTH-1 downto 0);        -- Frequency Tuning Word input.
+           POW_IN   : in unsigned(PHASE_WIDTH-1 downto 0);      -- Phase Offset Word.
+           OUT_SQ   : out std_logic;                            -- NCO's MSB in order to produce a square wave of FTW duration.
            SINE_OUT : out signed(AMP_WIDTH-1 downto 0) := (others => '0') -- DDS/NCO phase out for LUT.
            );
            -- Example: For f_clk=100 MHz, FTW_WIDTH=32:
            -- FTW_IN = round(f_out / 100e6 * 2^32)
+           -- where f_out is the desired output frequency.
 end DDS;
 
 architecture Behavioral of DDS is
